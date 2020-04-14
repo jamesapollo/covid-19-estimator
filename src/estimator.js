3043ece46ccf8ff720ac66e85b45a106dac3a101
James Apollo <apollohjames@gmail.com>
2020-04-14 19:41:45 +0300

    fixed estimation to 30 days

--- src/estimator.js
+++ src/estimator.js
@@ -1,4 +1,4 @@
-const covid19ImpactEstimator = (data, duration) => {
+const covid19ImpactEstimator = (data) => {
   // deconstruct the reportCases value from the data
   const { reportCases } = data;
 
@@ -10,15 +10,9 @@ const covid19ImpactEstimator = (data, duration) => {
   impact.currentlyInfected = reportCases * 10;
   severeImpact.currentlyInfected = reportCases * 50;
 
-  // get the duration factor by dividing by 3
-  let factor = duration / 3;
-  // convert to a whole number
-  factor = Math.ceil(factor);
-
-
   // calculate the infectionsByRequestedTime
-  impact.infectionsByRequestedTime = impact.currentlyInfected * (2 ** factor);
-  severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * (2 ** factor);
+  impact.infectionsByRequestedTime = impact.currentlyInfected * (2 ** 10);
+  severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * (2 ** 10);
 
   return {
     data,
