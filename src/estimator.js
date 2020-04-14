806096bbd9956a07bb6b490eef5fd47eed20043c
James Apollo <apollohjames@gmail.com>
2020-04-14 22:53:26 +0300

    challenge 3

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,18 @@
+const convertToDays = data => {
+  const { periodType, timeToElapse } = data;
+  
+  if (periodType === 'days') {
+    return timeToElapse* 1;
+  }
+  if (periodType === 'weeks') {
+    return timeToElapse * 7;
+  }
+  if (periodType === 'months') {
+    return timeToElapse * 30;
+  }
+}
+  
+
 const covid19ImpactEstimator = (data) => {
   // deconstruct the reportCases value from the data
   const {
@@ -38,11 +53,29 @@ const covid19ImpactEstimator = (data) => {
   severeImpact.hospitalBedsByRequestedTime = Math.ceil(hospitalBedsByRequestedTime)
   - severeImpact.severeCasesByRequestedTime;
 
+  const i_casesForICUByRequestedTime = impact.infectionsByRequestedTime * 0.05;
+  const sI_casesForICUByRequestedTime = severeImpact.infectionsByRequestedTime * 0.05;
+
+  const i_casesForVentilatorsByRequestedTime = impact.infectionsByRequestedTime * 0.02;
+  const sI_casesForVentilatorsByRequestedTime = severeImpact.infectionsByRequestedTime * 0.02;
+
+  impact.casesForICUByRequestedTime = Math.trunc(i_casesForICUByRequestedTime);
+  severeImpact.casesForICUByRequestedTime = Math.trunc(sI_casesForICUByRequestedTime);
+
+  impact.casesForVentilatorsByRequestedTime = Math.trunc(i_casesForVentilatorsByRequestedTime);
+  severeImpact.casesForVentilatorsByRequestedTime = Math.trunc(sI_casesForVentilatorsByRequestedTime);
+
+  impact.dollarsInFlight = (impact.infectionsByRequestedTime* 0.65)
+  * 1.5 * convertToDays(data);
+  severeImpact.dollarsInFlight = (severeImpact.infectionsByRequestedTime * 0.65)
+  * 1.5 * convertToDays(data);
+
   return {
     data,
     impact,
     severeImpact
   };
 };
+
 
 export default covid19ImpactEstimator;
