fb0a53983b654a8d289d0307c3a0671b370587f4
James Apollo <apollohjames@gmail.com>
2020-04-14 19:53:24 +0300

    fixed spelling of report cases

--- src/estimator.js
+++ src/estimator.js
@@ -1,14 +1,14 @@
 const covid19ImpactEstimator = (data) => {
   // deconstruct the reportCases value from the data
-  const { reportCases } = data;
+  const { reportedCases } = data;
 
   // initialize the impact and severeImpact properties for the output
   const impact = {};
   const severeImpact = {};
 
   // calculate the currently infected
-  impact.currentlyInfected = reportCases * 10;
-  severeImpact.currentlyInfected = reportCases * 50;
+  impact.currentlyInfected = reportedCases * 10;
+  severeImpact.currentlyInfected = reportedCases * 50;
 
   // calculate the infectionsByRequestedTime
   impact.infectionsByRequestedTime = impact.currentlyInfected * 1024;
